248c02327ff93e5b515e7c97c0e5fdc25b7d08e8
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2019-12-06 16:03:16 -0500

    Added more products and filters

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const products = [
         price: 15.99,
         quantity: 1,
         category: `clothing`,
-        color: `maroon`,
+        color: `Red`,
         size: `M`
     }
     ,
@@ -45,7 +45,7 @@ const products = [
         price: 27.00,
         quantity: 1,
         category: `clothing`,
-        color: `pour milk`,
+        color: `White`,
         size: `L`
     }
     ,
@@ -57,7 +57,7 @@ const products = [
         price: 99.00,
         quantity: 4,
         category: `clothing`,
-        color: `White`,
+        color: `Black`,
         size: `XL`
     }
     ,
@@ -86,77 +86,161 @@ const products = [
     }
     ,
     {
-        id: 3,
-        name: `Adidas Originals`,
-        image: "img/adidas originals.jpg",
-        description: `Kangaroo pocket.Drawcord-adjustable hood.Long sleeves with ribbed cuffs. 100% soft feel.`,
-        price: 54.00,
+        id: 8,
+        name: `FC Barcelona`,
+        image: "img/FC Barcelona.jpg",
+        description: `The FC Barcelona Hoodie has soft French terry fleece with team details to keep you comfortable while repping your team.`,
+        price: 48.00,
         quantity: 1,
         category: `clothing`,
-        color: `White`,
+        color: `Red`,
+        size: `L`
+    }
+    ,
+    {
+        id: 9,
+        name: `Chicago Bulls`,
+        image: "img/Chicago Bulls.jpg",
+        description: `Connect with your team in the Chicago Bulls Nike Spotlight NBA Hoodie, a pullover hoodie inspired by what pros wear at training. It's made from mid-weight, double-knit fabric that's soft yet durable and comfortable all around.`,
+        price: 84.00,
+        quantity: 1,
+        category: `clothing`,
+        color: `Red`,
+        size: `M`
+    }
+    ,
+    {
+        id: 10,
+        name: `NFL Raiders`,
+        image: "img/NFL Raiders.jpg",
+        description: `The Nike Circuit Logo Essential (NFL Raiders) Men's Pullover Hoodie pays tribute to your team with a logo detail on warm, comfortable fabric.`,
+        price: 190.00,
+        quantity: 1,
+        category: `clothing`,
+        color: `Black`,
         size: `XL`
     }
     ,
     {
-        id: 3,
-        name: `Adidas Originals`,
-        image: "img/adidas originals.jpg",
-        description: `Kangaroo pocket.Drawcord-adjustable hood.Long sleeves with ribbed cuffs. 100% soft feel.`,
-        price: 54.00,
+        id: 11,
+        name: `The North Face`,
+        image: "img/THe North Face.jpg",
+        description: `Whether he’s biking to school or playing ball in the park, our new and improved midweight hoodie features a cordless hood and cozy kangaroo pocket that’s deep enough to stash small souvenirs from the trails.`,
+        price: 50.00,
         quantity: 1,
         category: `clothing`,
-        color: `White`,
-        size: `XL`
+        color: `Grey`,
+        size: `M`
     }
     ,
     {
-        id: 3,
-        name: `Adidas Originals`,
-        image: "img/adidas originals.jpg",
+        id: 12,
+        name: `H&M Hooded Sweatshirt`,
+        image: "img/H&M.jpg",
         description: `Kangaroo pocket; Drawcord-adjustable hood.Long sleeves with ribbed cuffs. 100% soft feel.`,
-        price: 54.00,
+        price: 25.00,
         quantity: 1,
         category: `clothing`,
-        color: `White`,
+        color: `Black`,
+        size: `S`
+    }
+    ,
+    {
+        id: 13,
+        name: `Nike Navy Dallas`,
+        image: "img/Nike navy Dallas.jpg",
+        description: `Nike's Club Fleece Logo Pullover hoodie is a classic must-have piece in every Dallas Cowboys fan's closet.`,
+        price: 52.00,
+        quantity: 1,
+        category: `clothing`,
+        color: `Blue`,
+        size: `S`
+    }
+    ,
+    {
+        id: 14,
+        name: `Nike Dak Prescott`,
+        image: "img/Nike Dak Prescott.jpg",
+        description: `Men's Nike Dak Prescott Navy Dallas Cowboys Therma Jersey`,
+        price: 97.00,
+        quantity: 1,
+        category: `clothing`,
+        color: `Blue`,
+        size: `M`
+    }
+    ,
+    {
+        id: 15,
+        name: `Adidas Originals Lockup Hoodie`,
+        image: "img/Adidas Originals Lockup Hoodie.jpg",
+        description: `Nike's Club Fleece Logo Pullover hoodie is a classic must-have piece in every Dallas Cowboys fan's closet.`,
+        price: 69.99,
+        quantity: 1,
+        category: `clothing`,
+        color: `Blue`,
+        size: `M`
+    }
+    ,
+    {
+        id: 16,
+        name: `Nike Grey Dallas Cowboys`,
+        image: "img/Nike white dallas cowboys.jpg",
+        description: `Nike's Club Fleece Logo Pullover hoodie is a classic must-have piece in every Dallas Cowboys fan's closet.`,
+        price: 64.99,
+        quantity: 1,
+        category: `clothing`,
+        color: `Grey`,
+        size: `S`
+    }
+    ,
+    {
+        id: 17,
+        name: `Mitchell and Ness Grey Oakland`,
+        image: "img/Mitchell and Ness Grey Oakland.jpg",
+        description: `Secure the look of a true die-hard Oakland Raiders fan this season with this Center Piece pullover hoodie from Mitchell & Ness!`,
+        price: 44.00,
+        quantity: 1,
+        category: `clothing`,
+        color: `Grey`,
         size: `XL`
     }
     ,
     {
-        id: 3,
-        name: `Adidas Originals`,
-        image: "img/adidas originals.jpg",
-        description: `Kangaroo pocket; Drawcord-adjustable hood.Long sleeves with ribbed cuffs. 100% soft feel.`,
-        price: 54.00,
+        id: 18,
+        name: `Nike Navy Dallas`,
+        image: "img/Nike navy Dallas.jpg",
+        description: `Nike's Club Fleece Logo Pullover hoodie is a classic must-have piece in every Dallas Cowboys fan's closet.`,
+        price: 52.00,
         quantity: 1,
         category: `clothing`,
-        color: `White`,
-        size: `XL`
+        color: `Blue`,
+        size: `S`
     }
     ,
     {
-        id: 3,
-        name: `Adidas Originals`,
-        image: "img/adidas originals.jpg",
-        description: `Kangaroo pocket; Drawcord-adjustable hood.Long sleeves with ribbed cuffs. 100% soft feel.`,
-        price: 54.00,
+        id: 19,
+        name: `Nike Navy Dallas`,
+        image: "img/Nike navy Dallas.jpg",
+        description: `Nike's Club Fleece Logo Pullover hoodie is a classic must-have piece in every Dallas Cowboys fan's closet.`,
+        price: 52.00,
         quantity: 1,
         category: `clothing`,
-        color: `White`,
-        size: `XL`
+        color: `Blue`,
+        size: `S`
     }
     ,
     {
-        id: 3,
-        name: `Adidas Originals`,
-        image: "img/adidas originals.jpg",
-        description: `Kangaroo pocket; Drawcord-adjustable hood.Long sleeves with ribbed cuffs. 100% soft feel.`,
-        price: 54.00,
+        id: 20,
+        name: `Nike Grey Navy Dallas`,
+        image: "img/Nike navy Dallas.jpg",
+        description: `Nike's Club Fleece Logo Pullover hoodie is a classic must-have piece in every Dallas Cowboys fan's closet.`,
+        price: 74.99,
         quantity: 1,
         category: `clothing`,
-        color: `White`,
-        size: `XL`
+        color: `Grey`,
+        size: `L`
     }
-
+    
 ];
 
 function getProductsasHtml(product)
@@ -186,8 +270,35 @@ function displaySearchBar(){ /***************SEARCH BOX DYNAMIC/************/
         </div>`
 }
 
-/*************SIDEBAR NAV *******************/
+const filterForRed = () =>{
+    const redProducts = products.filter(c => c.color == "Red");
+    renderProductsFromArray(redProducts);
 
+}
+
+const filterForBlack = () => {
+    const blackProducts = products.filter(c => c.color == "Black");
+    renderProductsFromArray(blackProducts);
+
+}
+
+const filterForWhite = () => {
+    const whiteProducts = products.filter(c => c.color == "White");
+    renderProductsFromArray(whiteProducts);
+
+}
+
+const filterForGrey = () => {
+    const greyProducts = products.filter(c => c.color == "Grey");
+    renderProductsFromArray(greyProducts);
+
+}
+
+const filterForBlue = () => {
+    const blueProducts = products.filter(c => c.color == "Blue");
+    renderProductsFromArray(blueProducts);
+
+}
 
 const searchForProducts=() => {
     const search = document.getElementById("search-box").value;
@@ -214,6 +325,10 @@ window.addEventListener('load', () => {
     document.getElementById("search_bar").innerHTML = displaySearchBar();
     document.getElementById("search-box").addEventListener("input",searchForProducts);
     document.getElementById("courseView").addEventListener("click",viewAsTiles);
- 
+    document.getElementById("red").addEventListener("click",filterForRed);
+    document.getElementById("black").addEventListener("click",filterForBlack);
+    document.getElementById("blue").addEventListener("click",filterForBlue);
+    document.getElementById("grey").addEventListener("click",filterForGrey);
+    document.getElementById("white").addEventListener("click",filterForWhite);
 
 });
